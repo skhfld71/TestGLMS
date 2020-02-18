852e4a19a9a8701e04e27ddf9859df05b8790921
hero <skhfld71@gmail.com>
2020-02-19 04:26:16 +0900

    test3

--- code0.js
+++ code0.js
@@ -5,9 +5,9 @@ gdjs.SettingsCode.conditionTrue_0 = {val:false};
 gdjs.SettingsCode.condition0IsTrue_0 = {val:false};
 
 
-gdjs.SettingsCode.userFunc0x680d00 = function(runtimeScene) {
+gdjs.SettingsCode.userFunc0x692bb8 = function(runtimeScene) {
 "use strict";
-runtimeScene.setBackgroundColor(255,255,255);
+runtimeScene.setBackgroundColor(255,255,25);
 
 };
 gdjs.SettingsCode.eventsList0xb4be0 = function(runtimeScene) {
@@ -15,7 +15,7 @@ gdjs.SettingsCode.eventsList0xb4be0 = function(runtimeScene) {
 {
 
 
-gdjs.SettingsCode.userFunc0x680d00(runtimeScene);
+gdjs.SettingsCode.userFunc0x692bb8(runtimeScene);
 
 }
 
